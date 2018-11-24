311aa1e8389c5fed1a73eb2c0bfef89544ceb2b6
Torre Matthew <torre.matthew@gmail.com>
2018-11-24 13:29:02 -0500

    updates

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,12 @@
+let projArr = [ 
+    {
+    description: "My introduction to javascript programming came by way of simple client-side games.", 
+    projName: "Crystal Collector",
+    image: "./assets/images/collectorgameimage.jpg"
+    },
+
+]
+
+$(document).ready(function(){
+    $('.tabs').tabs();
+  });
