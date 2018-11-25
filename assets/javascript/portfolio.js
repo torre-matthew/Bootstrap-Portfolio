f1c755b3da72c0c40cb5ef605eaf732e816d36c5
Torre Matthew <torre.matthew@gmail.com>
2018-11-24 21:27:27 -0500

    more project area updates

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,9 +1,88 @@
-let projArr = [ 
-    {
-    description: "My introduction to javascript programming came by way of simple client-side games.", 
+let allProjArr = [ 
+{
+    description: "The concept of this game is pretty simple but the logic necessary to pull it off really helped reinforce fundemental concepts of javascript and programming in general.", 
     projName: "Crystal Collector",
-    image: "./assets/images/collectorgameimage.jpg"
-    },
+    image: "./assets/images/crystal.png",
+    gitHub: "test",
+    deployedUrl: "test"
+},
+{
+    description: "Fun trivia game that provided an opportunity to play with setInterval and setTimout javascript functions.", 
+    projName: "Pixar Trivia",
+    image: "./assets/images/triviagame.png",
+    gitHub: "TextTrackList",
+    deployedUrl: "test"
+},
+{
+    description: "Placeholder", 
+    projName: "Horror Movie Showdown",
+    image: "./assets/images/HorroMovieShowdown.png",
+    gitHub: "Placeholder",
+    deployedUrl: "Placeholder"
+},
+{
+    description: "Placeholder", 
+    projName: "Movie Car Pickup Service",
+    image: "./assets/images/MovieCarService.png",
+    gitHub: "PlaceHolder",
+    deployedUrl: "Placeholder"
+},
+{
+    description: "Placeholder", 
+    projName: "Movie Car Pickup Service",
+    image: "./assets/images/MovieCarService.png",
+    gitHub: "PlaceHolder",
+    deployedUrl: "Placeholder"
+},
+{
+    description: "Placeholder", 
+    projName: "Git Lit",
+    image: "Placeholder",
+    gitHub: "PlaceHolder",
+    deployedUrl: "Placeholder"
+},
+{
+    description: "Placeholder", 
+    projName: "Boba Me, Baby!!",
+    image: "Placeholder",
+    gitHub: "PlaceHolder",
+    deployedUrl: "Placeholder"
+},
+{
+    description: "Placeholder", 
+    projName: "Petal",
+    image: "Placeholder",
+    gitHub: "PlaceHolder",
+    deployedUrl: "Placeholder"
+},
+{
+    description: "Placeholder", 
+    projName: "Potter Alter-ego",
+    image: "Placeholder",
+    gitHub: "PlaceHolder",
+    deployedUrl: "Placeholder"
+},
+{
+    description: "Placeholder", 
+    projName: "Potter House Sorter",
+    image: "Placeholder",
+    gitHub: "PlaceHolder",
+    deployedUrl: "Placeholder"
+},
+{
+    description: "Placeholder", 
+    projName: "iDecide",
+    image: "Placeholder",
+    gitHub: "PlaceHolder",
+    deployedUrl: "Placeholder"
+},
+{
+    description: "My first javascript program!!!", 
+    projName: "Psychic Game",
+    image: "Placeholder",
+    gitHub: "PlaceHolder",
+    deployedUrl: "Placeholder"
+},
 
 ]
 
